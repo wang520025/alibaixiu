fbe839590bd5a34a555c1ee13f73d83101e737ab
wqf <wqf@qq.com>
2019-12-07 11:06:01 +0800

    筛选和分页提交

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -3,12 +3,12 @@ $.ajax({
 	type: 'get',
 	url: '/posts',
 	success: function (response) {
-        // console.log(response);
+        console.log(response);
         var html = template('postsTpl', response);
         // console.log(html);
 		$('#postsBox').html(html);
-		// var page = template('pageTpl', response);
-		// $('#page').html(page);
+		var page = template('pageTpl', response);
+		$('#page').html(page);
 	}
 });
 // 处理日期时间格式
@@ -17,3 +17,52 @@ function formateDate(date) {
 	date = new Date(date);
 	return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
 }
+// 分页
+function changePage (page) {
+	// alert(page);
+	// 向服务器端发送请求 获取文章列表数据
+	$.ajax({
+		type: 'get',
+		url: '/posts',
+		data: {
+			page: page
+		},
+		success: function (response) {
+			console.log(response);
+			var html = template('postsTpl', response);
+			$('#postsBox').html(html);
+			var page = template('pageTpl', response);
+			$('#page').html(page);
+		}
+	});
+}
+// 向服务器端发送请求 索要分类数据
+$.ajax({
+	type: 'get',
+	url: '/categories',
+	success: function (response) {
+		console.log(response)
+		var html = template('categoryTpl', {data: response});
+		console.log(html);
+		$('#categoryBox').html(html);
+	}
+})
+// 当用户进行文章列表筛选的时候
+$('#filterForm').on('submit', function () {
+	// 获取到管理员选择的过滤条件
+	var formData = $(this).serialize();
+	// 向服务器端发送请求 根据条件索要文章列表数据
+	$.ajax({
+		type: 'get',
+		url: '/posts',
+		data: formData,
+		success: function (response) {
+			var html = template('postsTpl', response);
+			$('#postsBox').html(html);
+			var page = template('pageTpl', response);
+			$('#page').html(page);
+		}
+	});
+	// 阻止表单默认提交行为
+	return false;
+});
